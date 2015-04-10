e9446360f13cdfcc9c78e0e1962140649d3bb5a4
Sam Aden <staden@mtu.edu>
2015-04-09 21:49:13 -0400

    new icon & can display location

--- app/src/main/java/org/mtri/navdraw/MainActivity.java
+++ app/src/main/java/org/mtri/navdraw/MainActivity.java
@@ -17,11 +17,15 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.support.v4.widget.DrawerLayout;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity
         implements NavigationDrawerFragment.NavigationDrawerCallbacks {
+
+
 
     /**
      * Fragment managing the behaviors, interactions and presentation of the navigation drawer.
@@ -46,6 +50,8 @@ public class MainActivity extends ActionBarActivity
         mNavigationDrawerFragment.setUp(
                 R.id.navigation_drawer,
                 (DrawerLayout) findViewById(R.id.drawer_layout));
+
+
     }
 
     @Override
@@ -153,65 +159,6 @@ public class MainActivity extends ActionBarActivity
     }
 
 
-    /*
-    * Launch submission activity
-     */
 
-//    private void SubmitAction() {
-//        Intent i = new Intent(MainActivity.this, SubmitActivity.class);
-//        startActivity(i);
-//    }
-
-//        int id = item.getItemId();
-
-    //noinspection SimplifiableIfStatement
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
-
-
-//        return super.onOptionsItemSelected(item);
-
-//    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
-
-        @Override
-        public void onAttach(Activity activity) {
-            super.onAttach(activity);
-            ((MainActivity) activity).onSectionAttached(
-                    getArguments().getInt(ARG_SECTION_NUMBER));
-        }
-    }
 
 }
--- app/src/main/java/org/mtri/navdraw/fragment1.java
+++ app/src/main/java/org/mtri/navdraw/fragment1.java
@@ -6,17 +6,61 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 /**
  * Created by sam on 3/23/15.
  */
-public class fragment1 extends android.support.v4.app.Fragment {
+public class fragment1 extends android.support.v4.app.Fragment implements View.OnClickListener{
+
+    Button btnShowLocation;
+
+    GPSTracker gps;
+
     View rootview;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+
         rootview = inflater.inflate(R.layout.fragment1_layout, container, false);
+
+
+        btnShowLocation = (Button) rootview.findViewById(R.id.show_location);
+
+        btnShowLocation.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                gps = new org.mtri.navdraw.GPSTracker(getActivity());
+
+                if (gps.canGetLocation()) {
+                    double latitude = gps.getLatitude();
+                    double longitude = gps.getLongitude();
+
+                    Toast.makeText(
+                            getActivity().getApplicationContext(),
+                            "Your Location is -\nLat: " + latitude + "\nLong: "
+                                    + longitude, Toast.LENGTH_LONG).show();
+                } else {
+                    gps.showSettingsAlert();
+                }
+            }
+        });
+
         return rootview;
+
+
+
+
     }
 
+
+
+    @Override
+    public void onClick(View v) {
+
+    }
 }
+
+
