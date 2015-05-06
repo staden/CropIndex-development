8149f89fd879110d3937c24e0566c4c0e26796c9
Sam Aden <staden@mtu.edu>
2015-05-06 03:08:19 -0400

    added RESET FORM button

--- app/src/main/java/org/mtri/navdraw/fragment0.java
+++ app/src/main/java/org/mtri/navdraw/fragment0.java
@@ -1,10 +1,12 @@
 package org.mtri.navdraw;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * Created by sam on 3/23/15.
@@ -15,6 +17,21 @@ public class fragment0 extends android.support.v4.app.Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         rootview = inflater.inflate(R.layout.fragment0_layout, container, false);
+
+        Button reset_btn = (Button) rootview.findViewById(R.id.reset_btn);
+        reset_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resetForm();
+            }
+            private void resetForm() {
+                Intent i = getActivity().getBaseContext().getPackageManager()
+                        .getLaunchIntentForPackage( getActivity().getBaseContext().getPackageName() );
+                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(i);
+            }
+        });
+
         return rootview;
     }
 
